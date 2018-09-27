4b55bfb6debdeb553402d0c9b87ac714e30e0558
askprod <43279284+askprod@users.noreply.github.com>
2018-09-27 22:21:32 +0200

    Typo + CP

--- pyramide-while-mario.rb
+++ pyramide-while-mario.rb
@@ -1,3 +1,17 @@
 # Maintenant, dernier programme, la pyramide de mario des champions. Nous allons te demander de faire la pyramide de Mario avec une boucle while, dans le sens de l'exercice 2.12 (pyramide).
 
 #J'utilisais déjà une boucle while ;)
+
+puts "Donne-moi un nombre entre 1 et 25, qui se transformera en super pyramide!"
+print ">"
+x = gets.chomp.to_i #Le nombre d'étages de la pyramide
+y = 1
+
+puts "Voici la pyramide:"
+while y < x
+    x.times {
+    print " " * (x - y) 
+    puts "#" * y
+    y += 1
+}
+end
